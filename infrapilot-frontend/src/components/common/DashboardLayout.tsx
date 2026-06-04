5bf679953c7b855fb441a41bc9a9fa973d4c1e7e
onkar45 <onkarrede45@gmail.com>
2026-06-04 15:54:15 +0530

    UI: Fixed navbar and Documents pagination

--- infrapilot-frontend/src/components/common/DashboardLayout.tsx
+++ infrapilot-frontend/src/components/common/DashboardLayout.tsx
@@ -28,7 +28,7 @@ const DashboardContent = ({ children }: Props) => {
       </div>
 
       <div className="flex-1 flex flex-col min-w-0 overflow-x-hidden">
-        <main className="flex-1 overflow-y-auto">{children || <Outlet />}</main>
+        <main className="flex-1 overflow-y-auto pt-16 relative">{children || <Outlet />}</main>
       </div>
     </div>
   );
